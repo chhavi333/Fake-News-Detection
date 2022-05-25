29bd49a27f893cbe4a7f62a910e724830a33713d
Pragya2010 <71688804+Pragya2010@users.noreply.github.com>
2022-05-25 21:31:06 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDzDjoZG6KqqVMkL7NV67xPJxfQpf8n-_E",
+    authDomain: "fake-news-detection-6ad01.firebaseapp.com",
+    projectId: "fake-news-detection-6ad01",
+    storageBucket: "fake-news-detection-6ad01.appspot.com",
+    messagingSenderId: "427474509716",
+    appId: "1:427474509716:web:c0a9b12158d6216dc3f981",
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
